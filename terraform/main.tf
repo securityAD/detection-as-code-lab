be846f9adeda4c3fed26421476a2736c822b9fe4
Aaron Diaz <90427861+securityAD@users.noreply.github.com>
2025-07-14 21:58:08 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,24 +25,28 @@ resource "aws_s3_bucket_policy" "cloudtrail_policy" {
 
 data "aws_iam_policy_document" "cloudtrail_bucket_policy" {
   statement {
+    sid    = "AWSCloudTrailAclCheck"
     effect = "Allow"
+
     principals {
       type        = "Service"
       identifiers = ["cloudtrail.amazonaws.com"]
     }
 
-    actions   = ["s3:GetBucketAcl"]
+    actions = ["s3:GetBucketAcl"]
     resources = [aws_s3_bucket.cloudtrail_bucket.arn]
   }
 
   statement {
+    sid    = "AWSCloudTrailWrite"
     effect = "Allow"
+
     principals {
       type        = "Service"
       identifiers = ["cloudtrail.amazonaws.com"]
     }
 
-    actions   = ["s3:PutObject"]
+    actions = ["s3:PutObject"]
     resources = ["${aws_s3_bucket.cloudtrail_bucket.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"]
 
     condition {
